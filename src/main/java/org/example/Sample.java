adb0e19198b668b816a96cbe7f4558fd8b1e4ec0
flick123 <vst1198@gmail.com>
2024-10-17 12:21:10 +0530

    c

---
+++ src/main/java/org/example/Sample.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.util.*;
+class Sample {
+    public static String encode(String m){
+        int length=m.length();
+        String result="";
+        LinkedHashMap<Character,Integer> lh=new LinkedHashMap<>();
+        for(int i=0;i<length;i++){
+            char c=m.charAt(i);
+            if(lh.containsKey(c)){
+                lh.put(c,lh.get(c)+1);
+            }
+            else{
+                lh.put(c,1);
+            }
+        }
+        for(Character ch: lh.keySet()){
+            result+=ch;
+            result+=lh.get(ch);
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        String s="aabbbyyycc";
+        String r=encode(s);
+        System.out.println(r);
+        String s1="aaaa";
+        String r1=encode(s1);
+        System.out.println(r1);
+        String s2="a";
+        String r2=encode(s2);
+        System.out.println(r2);
+
+    }
+}
